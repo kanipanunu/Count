883d7a4d99d1536c9c0307f2553d3e83f0d5b31f
megukanipan <megukanipan@192.168.100.136>
2014-11-15 17:48:04 +0900

    n桁まで対応

--- src/com/example/count/MainActivity.java
+++ src/com/example/count/MainActivity.java
@@ -10,59 +10,63 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	int n=0;
-	TextView textView,nabeatuText;
-	boolean nabeatu=false;
-	boolean nabecheck=false;
+	int n = 0;
+	TextView textView, nabeatuText;
+	boolean nabeatu = false;
+	boolean nabecheck = false;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		textView=(TextView) findViewById(R.id.textView);
-		nabeatuText=(TextView)findViewById(R.id.textView1);
+		textView = (TextView) findViewById(R.id.textView);
+		nabeatuText = (TextView) findViewById(R.id.textView1);
 	}
-	public void nabecheck(){
-		if((n/3*3)==n){
-			nabecheck=true;
-		}else if(n/10==3){
-			nabecheck=true;
-		}else if (n-n/10==3) {
-			nabecheck=true;
-		}else{
-			nabecheck=false;
+
+	public void nabecheck() {
+		if ((n / 3 * 3) == n) {
+			nabecheck = true;
+		} else if (String.valueOf(n).contains("3")) {
+			nabecheck = true;
+		} else {
+			nabecheck = false;
 		}
-		
-		if(nabeatu==true){
-			if(nabecheck==true){
+
+		if (nabeatu == true) {
+			if (nabecheck == true) {
 				textView.setTextColor(Color.RED);
-			}else{
+			} else {
 				textView.setTextColor(Color.BLACK);
 			}
 		}
 	}
+
 	public void plus(View v) {
 		n++;
-		textView.setText(""+n);
+		textView.setText("" + n);
 		nabecheck();
-}
+	}
+
 	public void mainasu(View v) {
 		n--;
-		textView.setText(""+n);
+		textView.setText("" + n);
 
 		nabecheck();
-}
+	}
+
 	public void clear(View v) {
-		n=0;
-		textView.setText(""+n);	
+		n = 0;
+		textView.setText("" + n);
 
 		textView.setTextColor(Color.BLACK);
-}
+	}
+
 	public void nabeatu(View v) {
-		nabeatu=!nabeatu;
-		if(nabeatu==true){
+		nabeatu = !nabeatu;
+		if (nabeatu == true) {
 			nabeatuText.setText("Ç»Ç◊Ç†Ç¬Ç‡Å[Ç«Ç»Ç§");
 			nabecheck();
-		}else{
+		} else {
 			nabeatuText.setText("");
 			textView.setTextColor(Color.BLACK);
 		}
